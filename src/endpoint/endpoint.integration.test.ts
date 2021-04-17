bb09ebf5ccb0097f2371257c58b0508ff65b9999
utkusarioglu <utkusarioglu@hotmail.com>
2021-04-17 10:13:43 +0000

    Update: add flavored integration tests
    * Get and Post tests with values that have flavors instead of
    primitives have been added

--- src/endpoint/endpoint.integration.test.ts
+++ src/endpoint/endpoint.integration.test.ts
@@ -1,15 +1,16 @@
 import { validateEndpoint, prepareEndpoint } from './endpoint';
 import { Get, Post } from '../index';
+import { uuid, mimetype, isoDate } from '../index';
 
 describe('validateEndpoint', () => {
-  it('uses GET as expected', () => {
+  it('uses GET', () => {
     type Feature = Get<'some/end/:point', { point: number }, {}>;
     const endpoint = 'some/end/:point';
     const validated = validateEndpoint<Feature['Endpoint']>(endpoint);
     expect(validated).toBe(endpoint);
   });
 
-  it('uses POST as expected', () => {
+  it('uses POST', () => {
     type Feature = Post<'some/end/:point', { point: number }, {}, {}>;
     const endpoint = 'some/end/:point';
     const validated = validateEndpoint<Feature['Endpoint']>(endpoint);
@@ -18,7 +19,7 @@ describe('validateEndpoint', () => {
 });
 
 describe('prepareEndpoint', () => {
-  it('uses GET as expected', () => {
+  it('uses GET', () => {
     type Feature = Get<
       '/some/:end/:point/:url',
       { point: number; end: string; url: boolean },
@@ -38,7 +39,27 @@ describe('prepareEndpoint', () => {
     expect(expected).toStrictEqual(prepared);
   });
 
-  it('uses POST as expected', () => {
+  it('uses GET with flavors', () => {
+    type Feature = Get<
+      '/some/:end/:point/:url',
+      { point: uuid; end: mimetype; url: isoDate },
+      {}
+    >;
+    const endpoint = '/some/:end/:point/:url';
+    const params = {
+      point: 'uuid',
+      end: 'mime-type',
+      url: 'isoDate',
+    };
+    const prepared = prepareEndpoint<
+      Feature['Endpoint'],
+      Feature['_get']['_req']['Params']
+    >(endpoint, params);
+    const expected = '/some/mime-type/uuid/isoDate';
+    expect(expected).toStrictEqual(prepared);
+  });
+
+  it('uses POST', () => {
     type Feature = Post<
       '/some/:end/:point/:url',
       { point: number; end: string; url: boolean },
@@ -56,6 +77,27 @@ describe('prepareEndpoint', () => {
       Feature['_post']['_req']['Params']
     >(endpoint, params);
     const expected = '/some/no/3/false';
+    expect(expected).toStrictEqual(prepared);
+  });
+
+  it('uses POST with flavors', () => {
+    type Feature = Post<
+      '/some/:end/:point/:url',
+      { point: uuid; end: mimetype; url: isoDate },
+      {},
+      {}
+    >;
+    const endpoint = '/some/:end/:point/:url';
+    const params = {
+      point: 'uuid',
+      end: 'mime-type',
+      url: 'isoDate',
+    };
+    const prepared = prepareEndpoint<
+      Feature['Endpoint'],
+      Feature['_post']['_req']['Params']
+    >(endpoint, params);
+    const expected = '/some/mime-type/uuid/isoDate';
     expect(expected).toStrictEqual(prepared);
   });
 });
