46b4449d25713803544c90710b69719fda9490a3
utkusarioglu <utkusarioglu@hotmail.com>
2021-04-17 07:08:11 +0000

    Chore: move types inside @types
    * This was done to manage the clutter in src

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export { validateEndpoint, prepareEndpoint } from './endpoint.methods';
 
-export type { Post, Get } from './endpoint.types';
-export type { Flavor } from './helper.types';
+export type { Post, Get } from './@types/endpoint.types';
+export type { Flavor } from './@types/helper.types';
 export type { Brand } from 'utility-types';
 
-export * from './mixin.types';
-export * from './flavor.types';
+export * from './@types/mixin.types';
+export * from './@types/flavor.types';
