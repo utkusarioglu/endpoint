786542d10fc4527f2cc1f874300855a03fa0aa6c
utkusarioglu <utkusarioglu@hotmail.com>
2021-04-23 09:35:44 +0000

    Feat: Create Meek types for all Rest endpoints
    * Meek endpoints are unopinionated, and gives the user to define
    whatever structure they want for the success and fail states of the
    response.
    
    * One other thing that changed in this commit was how the opinionated
    endpoint types such as Get and Post were defined. Now, these endpoints
    use their Meek counterparts to define their opinionated structure.

--- src/@types/rest.types.ts
+++ src/@types/rest.types.ts
@@ -68,7 +68,22 @@ export type GetMeek<
  * change its state). If you want to change data on the server, use POST,
  * PUT, PATCH or DELETE methods.
  */
-export type Head<Endpoint, ReqParams extends ReqParamsExtends> = {
+export type Head<Endpoint, ReqParams extends ReqParamsExtends> = HeadMeek<
+  Endpoint,
+  ReqParams
+>;
+
+/**
+ * Unopinionated Rest HEAD endpoint types hierarchy
+ *
+ * The HTTP HEAD method is used to request HTTP headers from the server.
+ * The HTTP HEAD request is identical to the GET request except that
+ * the server must not return the message body in the response. Requests
+ * using the HTTP HEAD method should only retrieve data (server must not
+ * change its state). If you want to change data on the server, use POST,
+ * PUT, PATCH or DELETE methods.
+ */
+export type HeadMeek<Endpoint, ReqParams extends ReqParamsExtends> = {
   Endpoint: Endpoint;
   _head: {
     _req: ReqStatesParams<ReqParams>;
@@ -90,12 +105,37 @@ export type Post<
   Endpoint extends string,
   ReqParams extends ReqParamsExtends,
   ReqBody extends ReqBodyExtends,
-  ResSuccessType
+  ResSuccessBody
+> = PostMeek<
+  Endpoint,
+  ReqParams,
+  ReqBody,
+  ResSuccessOpinionated<ResSuccessBody>,
+  ResFailOpinionated
+>;
+
+/**
+ * Unopinionated Rest POST endpoint types hierarchy
+ *
+ * The HTTP POST method is used to create or add a resource on the server.
+ * Typically, the POST request adds a new resource to the server, while the
+ * PUT request replaces an existing resource on the server. For example,
+ * the HTTP POST request method is used by browsers when submitting HTML
+ * form data to the server or when submitting data using jQuery/AJAX
+ * requests. Unlike GET and HEAD requests, the HTTP POST requests may
+ * change the server state.
+ */
+export type PostMeek<
+  Endpoint extends string,
+  ReqParams extends ReqParamsExtends,
+  ReqBody extends ReqBodyExtends,
+  ResSuccess extends ResSuccessExtends,
+  ResFail extends ResFailExtends
 > = {
   Endpoint: Endpoint;
   _post: {
     _req: ReqStatesParamsBody<ReqParams, ReqBody>;
-    _res: ResStatesOpinionated<ResSuccessType>;
+    _res: ResStatesMeek<ResSuccess, ResFail>;
   };
 };
 
@@ -111,12 +151,34 @@ export type Put<
   Endpoint extends string,
   ReqParams extends ReqParamsExtends,
   ReqBody extends ReqBodyExtends,
-  ResSuccessType
+  ResSuccessBody
+> = PutMeek<
+  Endpoint,
+  ReqParams,
+  ReqBody,
+  ResSuccessOpinionated<ResSuccessBody>,
+  ResFailOpinionated
+>;
+
+/**
+ * Unopinionated Rest PUT endpoint types hierarchy
+ *
+ * The HTTP PUT method is used to update an existing resource on the
+ * server, while the POST method creates or adds a resource on the server.
+ * Unlike GET and HEAD requests, the HTTP PUT request may change the server
+ * state.
+ */
+export type PutMeek<
+  Endpoint extends string,
+  ReqParams extends ReqParamsExtends,
+  ReqBody extends ReqBodyExtends,
+  ResSuccess extends ResSuccessExtends,
+  ResFail extends ResFailExtends
 > = {
   Endpoint: Endpoint;
   _put: {
     _req: ReqStatesParamsBody<ReqParams, ReqBody>;
-    _res: ResStatesOpinionated<ResSuccessType>;
+    _res: ResStatesMeek<ResSuccess, ResFail>;
   };
 };
 
@@ -133,12 +195,35 @@ export type Patch<
   Endpoint extends string,
   ReqParams extends ReqParamsExtends,
   ReqBody extends ReqBodyExtends,
-  ResSuccessType
+  ResSuccessBody
+> = PatchMeek<
+  Endpoint,
+  ReqParams,
+  ReqBody,
+  ResSuccessOpinionated<ResSuccessBody>,
+  ResFailOpinionated
+>;
+
+/**
+ * Unopinionated Rest PATCH endpoint types hierarchy
+ *
+ * The HTTP PATCH method is used to make partial changes to an existing
+ * resource. Typically, the PATCH method applies partial modifications
+ * to a resource, while the PUT method performs a complete replacement
+ * of the resource. Unlike GET and HEAD requests, the PATCH requests may
+ * change the server state.
+ */
+export type PatchMeek<
+  Endpoint extends string,
+  ReqParams extends ReqParamsExtends,
+  ReqBody extends ReqBodyExtends,
+  ResSuccess extends ResSuccessExtends,
+  ResFail extends ResFailExtends
 > = {
   Endpoint: Endpoint;
   _patch: {
     _req: ReqStatesParamsBody<ReqParams, ReqBody>;
-    _res: ResStatesOpinionated<ResSuccessType>;
+    _res: ResStatesMeek<ResSuccess, ResFail>;
   };
 };
 
@@ -158,11 +243,36 @@ export type Patch<
 export type Delete<
   Endpoint extends string,
   ReqParams extends ReqParamsExtends,
-  ResSuccessType
+  ResSuccessBody
+> = DeleteMeek<
+  Endpoint,
+  ReqParams,
+  ResSuccessOpinionated<ResSuccessBody>,
+  ResFailOpinionated
+>;
+
+/**
+ * Unopinionated Rest DELETE endpoint types hierarchy
+ *
+ * The HTTP DELETE method is used to delete a resource from the server.
+ * Unlike GET and HEAD requests, the DELETE requests may change the server
+ * state.
+ *
+ * Sending a message body on a DELETE request might cause some servers to
+ * reject the request. For this reason, this library does not allow you to
+ * define a body for your DELETE requests. You still can send data to the
+ * server using URL parameters. This is usually an ID of the resource you
+ * want to delete.
+ */
+export type DeleteMeek<
+  Endpoint extends string,
+  ReqParams extends ReqParamsExtends,
+  ResSuccess extends ResSuccessExtends,
+  ResFail extends ResFailExtends
 > = {
   Endpoint: Endpoint;
   _delete: {
     _req: ReqStatesParams<ReqParams>;
-    _res: ResStatesOpinionated<ResSuccessType>;
+    _res: ResStatesMeek<ResSuccess, ResFail>;
   };
 };
